f8d4f7dba8d14c338625cb975ccd976a41772e14
Pim Derks <info@pimderks.com>
2015-07-01 10:01:42 +0200

    Prevent duplicate instances of Triggers.

--- src/static/js/lib/TriggerFactory.js
+++ src/static/js/lib/TriggerFactory.js
@@ -3,15 +3,24 @@ var TriggerFactory = (function(){
     'use strict';
 
     var exports = function(){
+        this._manager = Manager.getInstance();
     };
 
     exports.prototype = {
 
         create: function(nodes){
 
+            var _this = this;
+
             [].slice.apply(nodes).forEach(function(node){
 
-                var name = trigger.nodeName.toLowerCase();
+                var name = node.nodeName.toLowerCase();
+
+                // check if this node is already a trigger
+                if(node.id && _this._manager.getToggleById(node.id)){
+                    return;
+                }
+
                 switch(name){
                     case 'select':
                         new TriggerSelect(node);
